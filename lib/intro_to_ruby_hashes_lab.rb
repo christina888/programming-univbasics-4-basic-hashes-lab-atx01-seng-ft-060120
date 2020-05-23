78bd7d79c803558fefc3a752f8a2160bd408ea39
Christina Chianelli <cechianelli@gmail.com>
2020-05-23 03:12:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -5,7 +5,7 @@ end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
-  female_singer = {
+  pop_artist = {
     :name => "Kim Petras",
     :occupation => "pop singer",
     :birthplace => "Germany"
